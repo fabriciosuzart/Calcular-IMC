829990b8d99448b1a66f475d1361f3992c67ae81
Fabricio Andrade <fabriciosuzart.andrade@gmail.com>
2024-01-31 15:09:00 -0300

    quaseFinish

--- App.js
+++ App.js
@@ -1,131 +1,120 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TouchableHighlight, TextInput, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableHighlight, TextInput, Image, KeyboardAvoidingView } from 'react-native';
 import { EventSubscriptionVendor } from 'react-native/Libraries/vendor/emitter/EventEmitter';
-import Tabela from './assets/Images/tabela-imc.png'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import Estilos from './Styles/Estilos.js'
+
 
 export default function calcic() {
 
   const [peso, setPeso] = useState(0)
   const [altura, setAltura] = useState(0)
   const [resultado, setResultado] = useState(0)
+  const [classi, setClassi] = useState('')
 
   const [visivel, setVisivel] = useState(false)
 
   const calcImc = () => {
     if (peso == 0 || !peso) {
       alert('Informe o Peso')
+      setVisivel(false)
       return
     }
     if (altura == 0 || !altura) {
       alert('Informe a altura')
+      setVisivel(false)
       return
     }
     const r = peso / (altura * altura)
     setResultado(r.toFixed(1))//define a quantidade de casas decimais 
+    calssificacao(r)
+  }
+
+  const calssificacao = (r) => {
+    if (r <= 18.5) {
+      setClassi('Abaixo do peso')
+    }
+    else if (r >= 18.6 && r <= 24.9) {
+      setClassi('Peso ideal')
+    }
+    else if (r >= 25 && r <= 29.9) {
+      setClassi('Acima do peso')
+    }
+    else if (r >= 30 && r <= 34.9) {
+      setClassi('Obseidade 1º')
+    }
+    else if (r >= 35 && r <= 39.9) {
+      setClassi('Obseidade 2º')
+    }
+    else if (r >= 40) {
+      setClassi('Obseidade 3º')
+    }
   }
 
   return (
-    <SafeAreaView style={estilos.corpo}>
-      <View style={estilos.bloco}>
-        <Text>Calculadora de IMC</Text>
+    <SafeAreaView style={Estilos.corpo}>
+      <View style={Estilos.titulo}>
+        <Text style={Estilos.titulo}>Calculadora de IMC</Text>
       </View>
-      <View style={estilos.bloco}>
-        <Text>Informe seu peso:</Text>
-        <TextInput
-          style={estilos.txt}
-          autoFocus={true}
-          keyboardType='numeric'
-          onChangeText={text => setPeso(text)}
-        ></TextInput>
-      </View>
+      <KeyboardAwareScrollView style={Estilos.corpo}>
+        <View style={Estilos.bloco}>
+          <Text>Informe seu peso:</Text>
+          <TextInput
+            style={Estilos.txt}
+            autoFocus={true}
+            keyboardType='numeric'
+            onChangeText={text => setPeso(text)}
+          ></TextInput>
+        </View>
 
-      <View style={estilos.bloco}>
-        <Text>Informe sua altura:</Text>
-        <TextInput
-          style={estilos.txt}
-          autoFocus={false}
-          keyboardType='numeric'
-          onChangeText={text => setAltura(text)}
-        ></TextInput>
-      </View>
+        <View style={Estilos.bloco}>
+          <Text>Informe sua altura:</Text>
+          <TextInput
+            style={Estilos.txt}
+            autoFocus={false}
+            keyboardType='numeric'
+            onChangeText={text => setAltura(text)}
+          ></TextInput>
+        </View>
 
-      <View style={estilos.bloco}>
+        <View style={Estilos.bloco}>
 
-        <TouchableHighlight
-          style={estilos.btnCalc}
-          onPress={() => {
-            calcImc()
-            setVisivel(true)
-          }
-          }
-        >
-          <Text style={estilos.txtBtn}>Calcular IMC</Text>
-        </TouchableHighlight>
+          <TouchableHighlight
+            style={Estilos.btnCalc}
+            onPress={() => {
+              calcImc(setVisivel(true))
+            }
+            }
+          >
+            <Text style={Estilos.txtBtn}>Calcular IMC</Text>
+          </TouchableHighlight>
 
-        <TouchableHighlight
-          style={estilos.btnCalc}
-          onPress={() => {
-            setVisivel(false)
-            setAltura('')
-            setPeso('')
-            setResultado('')
-          }
-          }
-        >
-          <Text style={estilos.txtBtn}>Limpar</Text>
-        </TouchableHighlight>
-      </View>
+          <TouchableHighlight
+            style={Estilos.btnCalc}
+            onPress={() => {
+              setVisivel(false)
+              setAltura('')
+              setPeso('')
+              setResultado('')
+            }
+            }
+          >
+            <Text style={Estilos.txtBtn}>Limpar</Text>
+          </TouchableHighlight>
+        </View>
 
-      <View style={estilos.bloco}>
-        <Text style={estilos.txtResult}>Resultado: {resultado}</Text>
-      </View>
-      <View>
-        {visivel && <Image
-          source={require('./assets/Images/tabela-imc.png')}
-          style={estilos.imgTable}
-        >
-        </Image>}
-      </View>
+        <View style={Estilos.centro}>
+          <Text style={Estilos.result}>Resultado: {resultado} Classificação: {classi}</Text>
+        </View>
+        <View>
+          {visivel && <Image
+            source={require('./assets/Images/tabela-imc.png')}
+            style={Estilos.imgTable}
+          >
+          </Image>}
+        </View>
+      </KeyboardAwareScrollView>
     </SafeAreaView>
   );
 }
-
-const estilos = StyleSheet.create({
-  corpo: {
-    padding: 10,
-    marginTop: 15
-  },
-  bloco: {
-    marginBottom: 20
-  },
-  txt: {
-    width: '100%',
-    borderWidth: 1,
-    borderColor: '#000',
-    padding: 10,
-    borderRadius: 10
-  },
-  btnCalc: {
-    backgroundColor: '#048',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 10,
-    borderRadius: 20,
-    marginBottom: 5
-  },
-  txtBtn: {
-    fontSize: 15,
-    textTransform: 'uppercase',
-    color: '#fff'
-  },
-  imgTable: {
-    resizeMode: 'stretch',
-    width: 340,
-    height: 200
-  },
-  txtResult: {
-    color: '#f00'
-  }
-})
-
---
+++ Styles/Estilos.js
@@ -0,0 +1,61 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  corpo: {
+    padding: 10,
+    marginTop: 15
+  },
+  bloco: {
+    marginBottom: 20
+  },
+  txt: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#000',
+    padding: 10,
+    borderRadius: 10
+  },
+  btnCalc: {
+    backgroundColor: '#048',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 20,
+    marginBottom: 5
+  },
+  txtBtn: {
+    fontSize: 15,
+    textTransform: 'uppercase',
+    color: '#fff'
+  },
+  imgTable: {
+    resizeMode: 'stretch',
+    width: 340,
+    height: 200,
+    marginBottom: 50
+  },
+  txtResult: {
+    color: '#f00'
+  },
+  titulo: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 5
+  },
+  result: {
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#000',
+    borderRadius: 15,
+    color: '#f00',
+    padding: 10,
+    marginBottom: 5
+  },
+  centro: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
